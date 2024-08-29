5fe4eea174abf718ebe313ae041db357ed73c8ab
Prashant Rana <101080988+ranaprashant19@users.noreply.github.com>
2024-08-29 19:37:54 +0530

    Update and rename Day 2-commet.java to Day-02:commet.java

--- Day-02:commet.java
+++ Day-02:commet.java
@@ -1,5 +1,10 @@
+//it is example of one line commet 
+import java.util.*;
 class hello{
   public static void main(string []args){
     System.out.println("hello world ! ");
   }
 }
+/*
+for multiline use this
+*/
