d85674a4ee82d7c595324ae35aeb7058bc0489ee
Prashant Rana <101080988+ranaprashant19@users.noreply.github.com>
2024-08-29 19:41:15 +0530

    comment in java
    how to use comment in java 
    1. one line comment 
    2. multiline comment

--- Day-02:commet.java
+++ Day-02:commet.java
@@ -1,5 +1,5 @@
 //it is example of one line commet 
-import java.util.*;
+import java.util.*; //import is keyword which use to include the package in your code and functionallity
 class hello{
   public static void main(string []args){
     System.out.println("hello world ! ");
