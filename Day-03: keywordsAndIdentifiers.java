d8c5270f4d83b9ee72f3c4ee0792d2d491fdfacd
Prashant Rana <101080988+ranaprashant19@users.noreply.github.com>
2024-09-11 11:16:18 +0530

    keywords and identifiers in java

---
+++ Day-03: keywordsAndIdentifiers.java
@@ -0,0 +1,13 @@
+import java.utill.*;
+public class keywords{
+  public static void main(String []Args){
+  /*
+  keywords cann't use as identifiers example are given below where
+  public is keywords and use like identifier 
+  they generate error 
+  */
+  //int public=2;
+  int x=2;
+  System.out.println(x);
+  }
+}
